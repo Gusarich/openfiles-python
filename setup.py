6bfd24ab9f61ca322ed58d9f48b683927afbb81e
Gusarich <danielsedovzzz@gmail.com>
2025-03-01 22:52:42 +0300

    feat: fix base url

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="openfiles",
-    version="1.0.1",
+    version="1.0.2",
     description="Python SDK for Openfiles API",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
